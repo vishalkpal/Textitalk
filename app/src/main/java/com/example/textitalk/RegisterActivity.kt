88a108e3db112c137d5c541065f122aee7b665ef
vishalkpal <dam.vish1992@gmail.com>
2021-05-28 12:33:21 +0530

    secound commit added firebase auth

--- app/src/main/java/com/example/textitalk/RegisterActivity.kt
+++ app/src/main/java/com/example/textitalk/RegisterActivity.kt
@@ -1,8 +1,12 @@
 package com.example.textitalk
 
+import android.app.Activity
 import android.content.Intent
+import android.graphics.drawable.BitmapDrawable
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import android.util.Log
 import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
@@ -27,9 +31,30 @@ class RegisterActivity : AppCompatActivity() {
         }
         profile_pic_button_register.setOnClickListener {
             Log.d("Main","Clicked on select photo")
+            val intent = Intent(Intent.ACTION_PICK)
+            intent.type = "image/*"
+            startActivityForResult(intent, 0)
         }
 
     }
+
+    // function of image slection
+    var SelectedPhotoUri: Uri? = null
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if(requestCode == 0 && resultCode == Activity.RESULT_OK && data != null ) {
+            Log.d("image", "selcted a image")
+
+            SelectedPhotoUri = data.data                                                 // getting the location of the image
+            val bitmap = MediaStore.Images.Media.getBitmap(contentResolver,SelectedPhotoUri )
+            val bitmapDrawable = BitmapDrawable(bitmap)
+            profile_pic_button_register.setBackgroundDrawable(bitmapDrawable)
+
+        }
+    }
+
+    // function to handle the email password authentication for registration process
     private fun performRegister(){
         val email = email_textview_register.text.toString()
         val password = password_edittext_register.text.toString()
@@ -47,10 +72,16 @@ class RegisterActivity : AppCompatActivity() {
                 if(!it.isSuccessful) return@addOnCompleteListener
                 Log.d("Main","sucessful: ${it.result?.user?.uid}")
 
+                Uploadselectedphoto()
+
             }
             .addOnFailureListener {
                 Log.d("Main","Failed to create: ${it.message}")
                 Toast.makeText(this, "Please enetr valid details ", Toast.LENGTH_LONG).show()
             }
+    }
+
+    private fun Uploadselectedphoto(){
+
     }
 }
