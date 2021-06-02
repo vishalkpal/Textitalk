9e68a2be269c593830c3f17d94e248b29eb25cf9
vishalkpal <dam.vish1992@gmail.com>
2021-06-02 18:04:24 +0530

    launcher activity handled and user_new_messege

--- app/src/main/java/com/example/textitalk/newmessegeActivity.kt
+++ app/src/main/java/com/example/textitalk/newmessegeActivity.kt
@@ -2,6 +2,10 @@ package com.example.textitalk
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.xwray.groupie.GroupAdapter
+import com.xwray.groupie.Item
+import com.xwray.groupie.ViewHolder
+import kotlinx.android.synthetic.main.activity_newmessege.*
 
 class newmessegeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -9,5 +13,24 @@ class newmessegeActivity : AppCompatActivity() {
         setContentView(R.layout.activity_newmessege)
 
         supportActionBar?.title ="Select User"
+
+        val adapter = GroupAdapter<ViewHolder>()
+
+        adapter.add(UserItem())
+        adapter.add(UserItem())
+        adapter.add(UserItem())
+        adapter.add(UserItem())
+        recyclerview_newmesseges.adapter =adapter
+    }
+}
+
+class UserItem: Item<ViewHolder>(){
+    override fun bind(viewHolder: ViewHolder, position: Int) {
+        // will be called in our list
+
+    }
+
+    override fun getLayout(): Int {
+        return R.layout.user_row_newmessege
     }
 }
